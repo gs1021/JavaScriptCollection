884b6b000a5f165d28fa7367fdce996eb1089f26
gaos@live.cn <gaos@live.cn>
2015-10-27 21:44:32 +0800

    add node express

---
+++ NodejsConsole/Lesson/lesson6.1.js
@@ -0,0 +1,8 @@
+﻿var foo = require('./lesson6.js');
+var should = require('should');
+
+describe('test/lesson6.test.js', function () {
+    it('should eq 55 when n==10', function () {
+        foo.foo(10).should.equal(55);
+    })
+})
---
+++ NodejsConsole/Lesson/lesson6.js
@@ -0,0 +1,18 @@
+﻿var foo = function (n) {
+    if (n === 0)
+        return 0;
+    if (n === 1)
+        return 1;
+    
+    return foo(n - 1) + foo(n - 2);
+};
+
+if (require.main === module) {
+    //若直接执行 lesson6.js 则会执行此方法
+    //从其他地方require则不会执行此处
+    var n = Number(process.argv[2]);
+    
+    console.log('fibonacci(' + n + ') is', foo(n));
+}
+
+exports.foo = foo;
