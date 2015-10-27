125ffe0d883862d009c29c277f1e6b1f78d76222
gscoy <gaos@live.cn>
2015-10-27 17:41:04 +0800

    1st commit

---
+++ JavaScriptParttenHost/单例模式/Singletion.js
@@ -0,0 +1,19 @@
+﻿var mySingletion = function () {
+    var privateVar = 'this is a private value';
+
+    function showPirvateValue() {
+        console.log(privateVar);
+    };
+
+    return {
+        PublicMethod: function () {
+            showPirvateValue();
+        },
+        PublicValue: 'this is a public value',
+    };
+};
+
+
+var singletion = new mySingletion();
+singletion.PublicMethod();
+singletion.PublicValue;
