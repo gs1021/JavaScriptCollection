9537a335e076359ebc6c15604afcae959afe14ab
gscoy <gaos@live.cn>
2015-10-28 16:40:59 +0800

    add file

---
+++ JavaScriptParttenHost/单例模式/Singletion2.js
@@ -0,0 +1,56 @@
+﻿var Singletion = function () {
+    var instance;
+    function Init() {
+        return {
+            PublicMethod: function () {
+                console.log('this is a public method');
+            },
+            PublicValue: 'this is a public value',
+        }
+    }
+
+    return {
+        getInstance: function () {
+            if (!instance) {
+                instance = Init();
+            }
+            return instance;
+        }
+    }
+}
+
+var single = new Singletion();
+single.getInstance().PublicMethod();
+single.getInstance().PublicValue;
+
+
+
+var singletionTester = (function () {
+    function Singletion(args) {
+        var args = args || {};
+
+        this.name = 'singletionTester';
+
+        this.pointX = args.pointX || 5;
+
+        this.pointY = args.poingY || 10;
+
+        var instance;
+
+        var _static = {
+            name: 'static_singletionTester',
+            getInstance: function (args) {
+                if (!instance) {
+                    instance = new Singletion(args);
+                }
+
+                return instance;
+            }
+        };
+
+        return _static;
+    }
+})();
+
+var single = singletionTester.getInstance({ pointX: 10 });
+console.log(single.pointX);
